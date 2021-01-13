6540bce3568601966f63ba263e5d37110db12447
mokulling <72229978+mokulling@users.noreply.github.com>
2021-01-12 22:36:27 -0600

    added additional script

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 var i = 0;
 var x = 0
 var y = 0
-var txt = 'Hi, I\'m Matt' /* The text */
-var txt2 = 'I\'m a web developer'
+const txt = 'Hi, I\'m Matt.'
+const txt2= 'I\'m a web developer.'
 var txt3 = 'I like creating concepts and building cool stuff for the web'
-var speed1 = 50; /* The speed/duration of the effect in milliseconds */
+var speed1 = 100; /* The speed/duration of the effect in milliseconds */
 var speed2 = 200
 var speed3 = 400
 var isDone1 = false
 var isDone2 = false
+let input = document.getElementById('type1')
+let input2 = document.getElementById('type2')
+
 
 function typeWriter1() {
   if (i < txt.length) {
-    document.getElementById("type1").innerHTML += txt.charAt(i);
+    input.innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter1, speed2);
-    }
+  }
+  setTimeout(typeWriter1, speed1)
+
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 typeWriter1()
-//typeWriter1()
