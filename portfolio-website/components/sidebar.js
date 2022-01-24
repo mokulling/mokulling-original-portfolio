f36aba4f76a2cf430350267bb79755cbefe07d12
mokulling <72229978+mokulling@users.noreply.github.com>
2022-01-24 16:48:55 -0600

    created skeleton

---
+++ portfolio-website/components/sidebar.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from './sidebar.module.css'
+
+export default function Sidebar() {
+  return (
+    <nav className={styles.nav}>
+
+<>
+<div>Home</div>
+<div>About</div>
+<div>Contact</div>
+
+
+
+
+</>
+
+
+    </nav>
+  )
+}
