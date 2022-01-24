428a398237f65ca3066a3b94a0a7e3fdd1c2f8c4
mokulling <72229978+mokulling@users.noreply.github.com>
2022-01-24 17:46:22 -0600

    added intro

---
+++ portfolio-website/components/intro.js
@@ -0,0 +1,33 @@
+import styles from './sidebar.module.css'
+
+
+export default function Intro() {
+
+return (
+
+<>
+<div className={styles.intro}>
+<div>Hello, I'm Matt</div>
+
+
+
+
+
+</div>
+
+</>
+
+
+
+
+
+
+)
+
+
+
+
+
+
+
+}
