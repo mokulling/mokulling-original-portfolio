9497347bd4f9e6ddf723c7424e3f6afe2a9d3e64
mokulling <72229978+mokulling@users.noreply.github.com>
2022-01-25 16:33:57 -0600

    added additional styles

--- portfolio-website/components/intro.js
+++ portfolio-website/components/intro.js
@@ -1,4 +1,5 @@
 import styles from './sidebar.module.css'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 
 export default function Intro() {
@@ -7,13 +8,15 @@ return (
 
 <>
 <div className={styles.intro}>
-<h1 className={styles.paragraph}>Hello, I'm Matt</h1> <br /><h2 className={styles.paragraph}>Let's work together to make cool things for the web. Please see below for examples of my work and see how to get in contact with me</h2>
+<h1 className={styles.paragraph}><ScrollAnimation animateIn='fadeIn'>
+Hello, I'm Matt</ScrollAnimation></h1> <br /><h2 className={styles.paragraph}>Let's work together to make cool things for the web. Please see below for examples of my work and see how to get in contact with me</h2>
 
 
 
 
 
 </div>
+
 
 </>
 
