375e2ee2c8cda9cb3c3fbccf1b04652129436aed
mokulling <72229978+mokulling@users.noreply.github.com>
2022-01-25 16:18:06 -0600

    added styles to intro

--- portfolio-website/components/intro.js
+++ portfolio-website/components/intro.js
@@ -7,8 +7,8 @@ return (
 
 <>
 <div className={styles.intro}>
-<div className={styles.paragraph}>Hello, I'm Matt <br />Let's work together to make cool things for the web. Please see below for examples of my work and see how to get in contact with me
-</div>
+<h1 className={styles.paragraph}>Hello, I'm Matt</h1> <br /><h2 className={styles.paragraph}>Let's work together to make cool things for the web. Please see below for examples of my work and see how to get in contact with me</h2>
+
 
 
 
