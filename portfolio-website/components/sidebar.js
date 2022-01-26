4d2326a43deea672140b7801eb4a9ae9d36f22b1
mokulling <72229978+mokulling@users.noreply.github.com>
2022-01-26 16:32:23 -0600

    added material ui

--- portfolio-website/components/sidebar.js
+++ portfolio-website/components/sidebar.js
@@ -1,12 +1,22 @@
 import Link from 'next/link'
 import styles from './sidebar.module.css'
+import * as React from 'react'
+import {Tooltip} from '@mui/material/Tooltip';
+
 
 export default function Sidebar() {
   return (
+
     <nav className={styles.nav}>
 
 <>
+{/* <Tooltip title="Home">Home</Tooltip> */}
 <div className={styles.nav_items}><div className={styles.nav_item}>Home</div>
+
+
+
+
+
 <div className={styles.nav_item}>About</div>
 <div className={styles.nav_item}>Contact</div>
 
